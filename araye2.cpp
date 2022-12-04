dc1ff686d91e9d6c3d2068389b6764e3a1ad13b7
nabiloo6060 <118072654+nabiloo6060@users.noreply.github.com>
2022-12-04 18:19:26 +0330

    Add files via upload

---
+++ araye2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+main(){
+int a[5],b[5];
+int n,m,c,d,e,f,g,h,i,t,r,p;
+for (i=0 ; i<=5 ; i++){
+	cout<<"please enter numbers for a[5]:";
+	cin >>n,m,c,d,e;
+	cout<<"please enter numbers for b[5]:";
+	cin>>f,g,h,t,r;
+	p=a[5];
+	a[5]=b[5];
+	p=b[5];
+	cout<<"a[5]:",a[5],"b[5];",b[5];
+	
+	
+}
+}
+
